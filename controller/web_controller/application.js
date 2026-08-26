5c8c5385fe88918593a404878d7bf53d31f24f30
Proactii <hello@proactii.com>
2026-08-26 12:03:34 +0530

    change footer slug

--- controller/web_controller/application.js
+++ controller/web_controller/application.js
@@ -1,14 +1,21 @@
-
 const { applications } = require("../../constants/data");
 
 const getAllApplications = async (req, res) => {
-  try {
-    res.render("applications", {
-        applications,
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+
+    try {
+
+        res.render("applications", {
+            applications,
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+
+    }
+
 };
 
 module.exports = { getAllApplications };
