6d4e4aefc003c45c84d44b1402d3da7d28b1ba5f
Proactii <hello@proactii.com>
2026-09-05 18:06:48 +0530

    chnage data of products

--- constants/data.js
+++ constants/data.js
@@ -534,7 +534,7 @@ const products = [
     type: "Automatic Pen Assembly Machine",
     image: "/shubhamimages/products/4semi.png",
     bannerImage: "/shubhamimages/banner/product_banner/4semi.png",
-    video: "/shubhamimages/video/4semi.mp4",
+    video: "/shubhamimages/video/semiautomatic.mp4",
     link: "/engineering-products/semi-automatic-assembly-machine-4up-sme-p-09-sme-r-09",
 
   keyFeatures: [
